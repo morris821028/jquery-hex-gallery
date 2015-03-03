8f232bedbce25331802a58a16cbb0cbe74a7ecfe
Morris Yang <morris821028@gmail.com>
2015-03-03 10:52:04 +0800

    clean some code

--- js/jquery.hex.gallery.js
+++ js/jquery.hex.gallery.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
-
+	var hex_header = '<div class="corner-1"></div><div class="corner-2"></div><div class="corner-3"></div>';
 	function getBackground(row, eachrow) {
-		var background = '';
+		var background = '',
+			background_image = '';
 		for (var i = 0; i < (row + 1) * (eachrow + 3); i++) {
-			var hexheader = '<div class="corner-1"></div><div class="corner-2"></div><div class="corner-3"></div>';
-			var showurl = '',
-				displayClass = 'hex';
-			if (i % (eachrow + 3) == 0)
-				displayClass += " hex-gap", row;
-			tag = hexheader + '<div class="inner"></div>';
-			tag = '<div class="' + displayClass + '" style="background-image: url(' + showurl + ');">' + tag + '</div>';
-			background += tag;
+			var displayClass = i % (eachrow + 3) ? 'hex' : 'hex hex-gap',
+				hex = '';
+			hex = hex_header + '<div class="inner"></div>';
+			hex = '<div class="' + displayClass + '" style="background-image: url(' + background_image + ');">' + hex + '</div>';
+			background += hex;
 		}
 		background = '<div class="hex-background">' + background + '</div>';
 		return background;
@@ -22,72 +20,71 @@ $(document).ready(function() {
 		return html;
 	}
 
-	function getBackLink(linkId, text) {
-		var hexheader = '<div class="corner-1"></div><div class="corner-2"></div><div class="corner-3"></div>';
-		var html = '',
-			tag = '',
-			showurl = '',
-			displayClass = 'hex hex-back';
-		var tag = hexheader + '<div class="inner" data-linkBtn="' + linkId + '"><h4>' + text + '</h4><hr /></a></div>';
-		tag = '<div class="' + displayClass + '" data-original="' + showurl + '" style="background-image: url(' + ');">' + tag + '</div>';
-		html += tag;
-		return html;
+	function getBackHex(linkId, text) {
+		var showurl = '',
+			displayClass = 'hex hex-back',
+			hex = '';
+		hex = hex_header + '<div class="inner" data-linkBtn="' + linkId + '"><h4>' + text + '</h4><hr /></a></div>';
+		hex = '<div class="' + displayClass + '" data-original="' + showurl + '" style="background-image: url(' + ');">' + hex + '</div>';
+		return hex;
 	}
 
-	function getAlsItem(images, options) {
+	function getAlsItem(albumImages, options) {
 		var html = '',
 			alsview = '',
 			eachrow = options.eachrow,
-			row = 0,
+			pagerow = 0,
 			index = 0,
 			imgCount = 0,
 			themePage = 0;
-		for (var i in images) {
-			var hexheader = '<div class="corner-1"></div><div class="corner-2"></div><div class="corner-3"></div>',
-				tag = '',
-				imgAttr = images[i],
+		for (var i in albumImages) {
+			var hex = '',
+				imgAttr = albumImages[i],
 				showurl = '',
 				displayClass = 'hex';
 
 			if (imgAttr['title']) {
-				var linkBtn = options.themePage[index];
-				tag = hexheader + '<div class="inner" data-linkBtn="' + linkBtn + '">' + imgAttr['title'] + '</div>';
+				var linkBtn = options.themePage[index],
+					title = imgAttr['title'];
+				hex = hex_header + '<div class="inner" data-linkBtn="' + linkBtn + '">' + title + '</div>';
 				if (imgAttr['class'])
 					displayClass += " " + imgAttr['class'];
 			} else {
-				for (var j in imgAttr) {
-					tag += '<p><a href="' + imgAttr[j] + '" class="hex-hover"><i class="icon-download"></i>' + j + '</a></p>';
-					showurl = imgAttr[j];
+				for (var attr in imgAttr) {
+					hex += '<p><a href="' + imgAttr[attr] + '" class="hex-hover"><i class="icon-download"></i>' + attr + '</a></p>';
+					showurl = imgAttr[attr];
 				}
-				tag = '<span class="hex-caption hex-simple-caption">' + tag + '</span> ';
-				tag = hexheader + '<div class="inner">' + tag + '</div>';
+				hex = '<span class="hex-caption hex-simple-caption">' + hex + '</span> ';
+				hex = hex_header + '<div class="inner">' + hex + '</div>';
 			}
 
-			if (imgCount % (eachrow + 1) == 0) {
+			var isAnotherPage = imgCount % (eachrow + 1) == 0;
+
+			if (isAnotherPage) {
 				if (html.length > 0) {
 					options.alsPage++;
-					html += getBackLink(1, "NEXT");
-					html = getHexContainer(html, row, eachrow, -options.alsPage);
+					html += getBackHex(1, "NEXT");
+					html = getHexContainer(html, pagerow, eachrow, -options.alsPage);
 					alsview += html;
 					imgCount = 0;
-					row = 0;
+					pagerow = 0;
 					themePage++;
 				}
 				displayClass += " hex-gap";
 				html = '';
-				row++;
+				pagerow++;
 			}
 			index++;
 			imgCount++;
 			displayClass += ' lazy';
-			tag = '<div class="' + displayClass + '" data-original="' + showurl + '" style="background-image: url(' + ');">' + tag + '</div>';
-			html += tag;
+			hex = '<div class="' + displayClass + '" data-original="' + showurl + '" style="background-image: url(' + ');">' + hex + '</div>';
+			html += hex;
 		}
 		if (html.length > 0) {
 			options.alsPage++;
 			if (!options.head)
-				html += getBackLink(-options.alsPage, "THIS END");
-			html = getHexContainer(html, row, eachrow, -options.alsPage);
+				html += getBackHex(-options.alsPage, "THIS END");
+			html = getHexContainer(html, pagerow, eachrow, -options.alsPage);
 			alsview += html;
 			themePage++;
 		}
@@ -95,12 +92,12 @@ $(document).ready(function() {
 		options.index++;
 		return alsview;
 	}
+
 	$("script[type='text/hex-gallery']").each(function(index, value) {
 		var $container = $(this),
 			$widget,
 			content = $container.text();
-		content = JSON.parse(content);
-		var data = {
+		var _config = {
 			alsPage: 0,
 			index: 1,
 			eachrow: 7,
@@ -109,17 +106,20 @@ $(document).ready(function() {
 		};
 		var album = [],
 			alsItem = '';
+
+		content = JSON.parse(content);
+
 		for (var i in content['album'])
 			album.push(content['album'][i]['cover']);
 		for (var i in content['album'])
-			alsItem += getAlsItem(content['album'][i]['photo'], data);
-		data.themePage.unshift(1);
-		for (var i = 0, sum = 0; i < data.themePage.length; i++) {
-			sum += data.themePage[i];
-			data.themePage[i] = sum;
+			alsItem += getAlsItem(content['album'][i]['photo'], _config);
+		_config.themePage.unshift(1);
+		for (var i = 0, sum = 0; i < _config.themePage.length; i++) {
+			sum += _config.themePage[i];
+			_config.themePage[i] = sum;
 		}
-		data.head = true;
-		album = getAlsItem(album, data);
+		_config.head = true;
+		album = getAlsItem(album, _config);
 		alsItem = album + alsItem;
 
 		var alsview = '<div class="als-viewport"><ul class="als-wrapper">' + alsItem + '</ul></div>',
@@ -131,7 +131,7 @@ $(document).ready(function() {
 		$widget = $(html);
 
 		$container.after($widget);
-		
+
 		$("div.lazy").lazyload({});
 
 		$("img.lazy").lazyload({});
